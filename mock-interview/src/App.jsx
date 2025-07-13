b04ad7fe8d4826989be9510815e8ccd0e0eba389
Dipti <singh236dipti@gmail.com>
2025-07-13 12:12:05 +0530

    Project Setup

---
+++ mock-interview/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import AIInterviewHero from './components/AIInterviewHero'
+import FeatureSection from './components/FeatureSection'
+import Navbar from './components/Navbar'
+function App() {
+
+  return (
+    <>
+    <Navbar />
+<AIInterviewHero />
+<FeatureSection />
+    </>
+  )
+}
+
+export default App
---
+++ mock-interview/src/Approutes.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import AIInterviewHero from './components/AIInterviewHero';
+function Approutes() {
+  return (
+    <Router>
+      <ScrollToTop />
+      <Navbar />
+      <SearchPage />
+      <Routes>
+        {/* Main Routes */}
+        <Route path="/bar" element={<Navbar />} />
+        <Route path="/hel" element={<AIInterviewHero />} />
+          </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default Approutes;
