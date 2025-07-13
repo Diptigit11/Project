6bdf4b26feb031bd9e91892e20092c14b44fd8f9
Dipti <singh236dipti@gmail.com>
2025-07-13 17:31:14 +0530

    added page for interview setup

--- mock-interview/src/App.jsx
+++ mock-interview/src/App.jsx
@@ -5,19 +5,19 @@ import Navbar from './components/Navbar';
 import AIInterviewHero from './components/AIInterviewHero';
 import Footer from './components/Footer';
 import FeatureSection from './components/FeatureSection';
+import InterviewSetup from './components/InterviewSetup';
 
 function Approutes() {
   return (
     <>
       <Navbar />
-      <main className="mt-[70px] sm:mt-[80px]">
       <Routes>
         <Route path="/" element={<><AIInterviewHero /><FeatureSection/> </>} />
         <Route path="/bar" element={<Navbar />} />
         <Route path="/hel" element={<AIInterviewHero />} />
         <Route path="/about-us" element={<FeatureSection />} />
+        <Route path="/interview" element={<InterviewSetup />} />
       </Routes>
-      </main>
       <Footer />
     </>
   );
