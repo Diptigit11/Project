0799e0b3bd65eef2f4c6a36e9561efa4a1f7304f
Dipti <singh236dipti@gmail.com>
2025-07-13 14:06:17 +0530

    fixes  route isses

--- mock-interview/src/App.jsx
+++ mock-interview/src/App.jsx
@@ -1,18 +1,19 @@
-import './App.css'
-import AIInterviewHero from './components/AIInterviewHero'
-import FeatureSection from './components/FeatureSection'
-import Footer from './components/Footer'
-import Navbar from './components/Navbar'
-function App() {
-
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import AIInterviewHero from './components/AIInterviewHero';
+import Footer from './components/Footer';
+function Approutes() {
   return (
-    <>
-    <Navbar />
-<AIInterviewHero />
-<FeatureSection />
-<Footer />
-    </>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/bar" element={<Navbar />} />
+        <Route path="/hel" element={<AIInterviewHero />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default Approutes;
